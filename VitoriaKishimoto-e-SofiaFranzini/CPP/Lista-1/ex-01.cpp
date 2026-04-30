e5c502db39576f44265c260a75e946ad6b61ea2c
Grupo da vitória, gabriela, sofia e giovanna :) <codegirls.etec@gmail.com>
2026-04-30 14:58:35 -0300

    Remove exercise header comment from ex-01.cpp

--- VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-1/ex-01.cpp
+++ VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-1/ex-01.cpp
@@ -3,7 +3,6 @@ Data: 23/04/2026
 Autores: Vitória Kishimoto e Sofia Franzini
 Descrição:Ler um número inteiro. Verificar e exibir se o número é primo.
 */
----
 
 #include <iostream>
 using namespace std;
