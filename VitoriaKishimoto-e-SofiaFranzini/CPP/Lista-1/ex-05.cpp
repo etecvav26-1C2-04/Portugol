b4634a0fe65632810b8d66da5711aac99f8a274a
Vitória Kishimoto <kishimoto.vitoria@gmail.com>
2026-05-05 16:18:55 -0300

    Create ex-05.cpp

---
+++ VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-1/ex-05.cpp
@@ -0,0 +1,28 @@
+/*
+Data: 05/05/2026
+Autores: Vitória Kishimoto e Sofia Franzini
+Descrição: Ler um número para a parada final e outro número que 
+representa um múltiplo. Exibir os múltiplos do número lido de 1 
+até o número final lido.
+/*
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int limite, base;
+
+    cout << "Digite o numero final: ";
+    cin >> limite;
+
+    cout << "Digite o numero base: ";
+    cin >> base;
+
+    for (int i = 1; i <= limite; i++) {
+        if (i % base == 0) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
