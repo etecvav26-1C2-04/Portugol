9039b625513f75f6790b2040bc6e4158bf31021a
Vitória Kishimoto <kishimoto.vitoria@gmail.com>
2026-05-05 12:37:05 -0300

    Create ex-02.cpp

---
+++ VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-1/ex-02.cpp
@@ -0,0 +1,24 @@
+/*
+Data: 05/05/2026
+Autores: Vitória Kishimoto e Sofia Franzini
+Descrição:Ler um número inteiro, calcular e exibir separadamente as potências de 2^0 até 2^(esse número).
+/* 
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Digite um numero: ";
+    cin >> num;
+
+    for (int i = 0; i <= num; i++) {
+        long long resultado = pow(2, i);
+        cout << "2 elevado a " << i << " = " << resultado << endl;
+    }
+
+    return 0;
+}
