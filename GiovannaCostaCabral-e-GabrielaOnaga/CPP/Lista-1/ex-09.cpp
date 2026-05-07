4179d5a11159132a063818fb565be00cb0ce270a
giovanna <giovannaccabral2@gmail.com>
2026-05-07 12:40:17 -0300

    Atualizar o ex-09.cpp

--- GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-1/ex-09.cpp
+++ GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-1/ex-09.cpp
@@ -1,1 +1,38 @@
-oi
+/*
+Data: 07/05/2026
+Autores: Gabriela Onaga e Giovanna Costa
+Descrição: Ler um número entre 50 e 100. Exibir a sequência de Fibonacci até esse número.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int limite;
+
+    cin >> limite;
+
+    if (limite < 50 || limite > 100) {
+        cout << "Numero invalido!" << endl;
+        return 0;
+    }
+
+    int numeroA = 0;
+    int numeroB = 1;
+    int proximo;
+
+    cout << numeroA << " " << numeroB << " ";
+
+    proximo = numeroA + numeroB;
+
+    while (proximo <= limite) {
+        cout << proximo << " ";
+
+        numeroA = numeroB;
+        numeroB = proximo;
+        proximo = numeroA + numeroB;
+    }
+
+    return 0;
+}
